020baf4be5786b6615cbf415deb59f52748913b8
Maxim <caliber1997@gmail.com>
2016-12-21 01:21:11 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="formal_verifier",
+    version="1.0.0",
+    author="Maksym Semikin",
+    author_email="maksym.semikin@gmail.com",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[
+        'flask'
+    ]
+)
